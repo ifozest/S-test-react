495b101a1116629412c47c9018de1ef7f81a60e6
Ifozest <aemaeth4@gmail.com>
2017-10-23 21:25:31 +0300

    init

--- dist/vendor.f027df178bed5e4f38c9.js
+++ dist/vendor.f027df178bed5e4f38c9.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "ec0acf8c342656b32c1e"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "f027df178bed5e4f38c9"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
